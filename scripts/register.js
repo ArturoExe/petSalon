1eb26d6853828f18e16d0be1e6450be97ac8a69a
ArturoExe <amarturin15@gmail.com>
2022-02-05 12:31:45 -0800

    Register function
    Function to push new pets to the array pets

--- scripts/register.js
+++ scripts/register.js
@@ -9,63 +9,67 @@ let salon={
         open:"9:00 am",
         close:"8:00 pm"
     },
-    pets:[
-    {
-        name:"Scooby",
-        age:60,
-        gender:"Male",
-        breed:"Dane",
-        service:"Grooming",
-        owner:"Shaggy",
-        phone:"555-555-555"
-
-    },
-    {
-        name:"Nala",
-        age:30,
-        gender:"Female",
-        breed:"German-Shepard",
-        service:"Bath",
-        owner:"Michael",
-        phone:"555-222-555"
-
-    },
-    {
-        name:"Nasus",
-        age:10,
-        gender:"Male",
-        breed:"Pug",
-        service:"Grooming",
-        owner:"Manuel",
-        phone:"555-555-333"
-
-    },
-    {
-        name:"Ares",
-        age:55,
-        gender:"Female",
-        breed:"Retriever",
-        service:"Nail Clipping",
-        owner:"Jose",
-        phone:"666-555-555"
-
-    },
-
-]
+    pets:[]
 
 }
 
 
-function displayPetNames(){
+// function displayPetNames(){
 
-  salon.pets.forEach((element)=> {
-    console.log(element.name);
-  });
+//   salon.pets.forEach((element)=> {
+//     console.log(element.name);
+//   });
     
-//   alert(`They are ${salon.pets.length} on the line`)
+// //   alert(`They are ${salon.pets.length} on the line`)
+// }
+
+// // displayPetNames();
+
+
+//Create the constructor
+class Pets{
+
+    constructor(name,age,gender){
+
+        this.name=name;
+        // this.breed.breed;
+        this.age=age;
+        this.gender=gender
+        // this.service=service;
+        // this.owner=onwer;
+    }
+
+
 }
 
-// displayPetNames();
+
+
+
+let register = () => {
+
+    let name=document.getElementById("txtpetName").value;
+    let age=document.getElementById("txtAge").value;
+    let gender=document.getElementById("rdbMale").checked;
+   //check if the gender
+    if(gender==true){
+        gender="Male"
+    }else
+    {
+        gender="Female"
+    }
+  
+    //Create new pet
+    let newPet=new Pets(name,age,gender)
+
+    //Push the pet to the pets array
+    salon.pets.push(newPet);
+
+    //log the pet info
+    console.log(newPet);
+    console.log(salon.pets);
+
+    
+};
 
 
 
