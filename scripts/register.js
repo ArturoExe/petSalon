9bb1f069823622f8b1c426238f266953d69eea77
ArturoExe <amarturin15@gmail.com>
2022-02-08 14:07:36 -0800

    Pet Card Creation

--- scripts/register.js
+++ scripts/register.js
@@ -23,13 +23,12 @@ function displayPetNames(){
    alert(`They are ${salon.pets.length} on the line`)
 }
 
-displayPetNames();
 
 
 //Create the constructor
 class Pets{
-
-    constructor(name,age,breed,service,gender,date,ownerInfo){
+    
+    constructor(name,age,breed,service,gender,date,ownerName,ownerPhone,ownerAddress){
 
         this.name=name;
         this.breed=breed;
@@ -37,7 +36,11 @@ class Pets{
         this.gender=gender
         this.service=service;
         this.date=date;
-        this.ownerInfo=ownerInfo;
+        this.ownerInfo={
+            name:ownerName,
+            phone:ownerPhone,
+            address:ownerAddress
+        };
     }
 
 
@@ -47,14 +50,16 @@ class Pets{
 
 
 let register = () => {
+    //Capture the values from the form
+    document.getElementById("petList").innerHTML="";
     let form=document.getElementById("petForm");
     let name=document.getElementById("txtpetName").value;
     let age=document.getElementById("txtAge").value;
     let breed=document.getElementById("txtBreed").value;
-    let service=document.getElementById("txtService").value;
+    let service=document.getElementById("selService").value;
     let gender=document.getElementById("rdbMale").checked;
-    let date=document.getElementById("txtDate").value;    
-    let ownerName=document.getElementById("txtownerName").value;
+    let date=document.getElementById("txtDate").value; 
+    let ownerName=document.getElementById("txtownerName").value;  
     let ownerPhone=document.getElementById("txtownerPhone").value;
     let ownerAddress=document.getElementById("txtownerAddress").value;
 
@@ -68,19 +73,84 @@ let register = () => {
   
    
     //Create new pet
-    let newPet=new Pets(name,age,breed,service,gender,date,{ ownerName,ownerPhone,ownerAddress})
+    let newPet=new Pets(name,age,breed,service,gender,date,ownerName,ownerPhone,ownerAddress)
 
     //Push the pet to the pets array
     salon.pets.push(newPet);
 
     //log the pet info
-    console.log(newPet);
-    console.log(salon.pets);
+    // console.log(newPet);
+    // console.log(salon.pets);
     alert("Pet Registered");
+
+    //clear form
     form.reset();
+
+    //Display added cards
+    printPetCard();
 }
 
+//Displays the registered pet on the table
+let printPetCard = () => {
+    //tarvel the array
+
+    for (let i = 0; i < salon.pets.length; i++) {
+        //create the card
+      
+    //append the tmp into the HTML
+    document.getElementById("petList").innerHTML+=createCard(salon.pets[i]);
+    }
+
+
+};
+
+
+let createCard = (pet) => {
+    //create the card with all the data
+    return `<div class="pet-card">
+        <h3>Name:${pet.name}</h3>
+        <p>Age:${pet.age}</p>
+        <p>Breed:${pet.breed}</p>
+        <p>Service:${pet.service}</p>
+        <p>Gender:${pet.gender}</p>
+        <p>Appointment:${pet.date}</p>
+
+        <h3>Owner Information</h3>
+        <p>${pet.ownerInfo.name}</p>
+        <p>${pet.ownerInfo.phone}</p>
+        <p>${pet.ownerInfo.address}</p>
+    </div>`;
+};
+
+//initial load on the webpage
+let init = () => {
+  
+    let soby=new Pets("SOBY",50,"Dane","Haircut","Male","none","Daniel","5555-444-444","Ave.orange");
+    let nala=new Pets("Nala",10,"golden","Haircut","Female","Today","Joe","5522-444-444","Ave.Russell");
+    salon.pets.push(soby,nala);
+    displayPetNames();
+    printPetCard();
+};
+
+
+window.onload=init;
 
 
 
 
+//Controls the hover color on the select-dropdown-menu 
+function select_option(){
+   var selectBox = document.getElementById("selService");
+   $size = selectBox.size;
+   $set_size = 4;
+   if ($size == $set_size) {
+     selectBox.size = 1;
+     selectBox.style.overflow = 'hidden';
+   } else {
+     selectBox.size = $set_size;
+     selectBox.style.height = 'auto';
+     selectBox.style.overflow = 'auto';
+   }
+   var selectedOptionTop = selectBox.options[selectBox.selectedIndex].offsetTop;
+   selectBox.scrollTop = selectedOptionTop;
+ }
